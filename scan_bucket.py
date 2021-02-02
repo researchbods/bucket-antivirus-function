9bd7f599fc64ced84f5e2fa3557b27edb67713f5
AKay <andybkay@icloud.com>
2021-02-02 10:08:00 +0000

    Changes to scan_bucket.py to include start_date, end_date and report functions

--- scan_bucket.py
+++ scan_bucket.py
@@ -23,15 +23,18 @@ import boto3
 
 from common import AV_STATUS_METADATA
 from common import AV_TIMESTAMP_METADATA
+from datetime import datetime, timedelta
 
 
 # Get all objects in an S3 bucket that have not been previously scanned
-def get_objects(s3_client, s3_bucket_name, limit):
+def get_objects(s3_client, s3_bucket_name, limit, start_date, end_date):
 
     s3_object_list = []
+    count = 0
 
     s3_list_objects_result = {"IsTruncated": True}
     while s3_list_objects_result["IsTruncated"]:
+        print(f"Objects checked: {count}")
         print(f"Update: Objects to be scanned = {len(s3_object_list)}")
         s3_list_objects_config = {"Bucket": s3_bucket_name}
         continuation_token = s3_list_objects_result.get("NextContinuationToken")
@@ -43,11 +46,16 @@ def get_objects(s3_client, s3_bucket_name, limit):
         if limit:
             if len(s3_object_list) >= limit:
                 break
+        
         for key in s3_list_objects_result["Contents"]:
+            count += 1
             key_name = key["Key"]
-            # Don't include objects that have been scanned
-            if not object_previously_scanned(s3_client, s3_bucket_name, key_name):
-                s3_object_list.append(key_name)
+            upload_date = key["LastModified"].replace(tzinfo=None)
+
+            if upload_date >= start_date and upload_date <= end_date:
+                # Don't include objects that have been scanned
+                if not object_previously_scanned(s3_client, s3_bucket_name, key_name):
+                    s3_object_list.append(key_name)
 
     return s3_object_list
 
@@ -88,8 +96,19 @@ def format_s3_event(s3_bucket_name, key_name):
     }
     return s3_event
 
+def parse_date(timestamp):
+    return datetime.utcfromtimestamp(timestamp)
 
-def main(lambda_function_name, s3_bucket_name, profile, limit):
+def export_json(s3_object_list):
+    
+    f = open('file_export.json', 'w')
+
+    f.write(json.dumps({"files": s3_object_list}))
+
+    f.close
+    print("Exported")
+
+def main(lambda_function_name, s3_bucket_name, profile, limit, start_time=None, end_time=None, report=None, export_file=None):
     # Verify the lambda exists
     sess = boto3.session.Session(profile_name=profile)
     lambda_client = sess.client("lambda")
@@ -99,21 +118,38 @@ def main(lambda_function_name, s3_bucket_name, profile, limit):
         print("Lambda Function '{}' does not exist".format(lambda_function_name))
         sys.exit(1)
 
-    # Verify the S3 bucket exists
-    s3_client = sess.client("s3")
-    try:
-        s3_client.head_bucket(Bucket=s3_bucket_name)
-    except Exception:
-        print("S3 Bucket '{}' does not exist".format(s3_bucket_name))
-        sys.exit(1)
+    if export_file:
+        export = open(export_file, 'r')
+        json_import = json.loads(export.read())["files"]
+        for key_name in json_import:
+            scan_object(lambda_client, lambda_function_name, s3_bucket_name, key_name)
 
-    # Scan the objects in the bucket
-    s3_object_list = get_objects(s3_client, s3_bucket_name, limit)
-    if limit:
-        s3_object_list = s3_object_list[: min(limit, len(s3_object_list))]
-        print(f"Final: Objects to be scanned: {len(s3_object_list)}")
-    for key_name in s3_object_list:
-        scan_object(lambda_client, lambda_function_name, s3_bucket_name, key_name)
+    else:
+
+        # Verify the S3 bucket exists
+        s3_client = sess.client("s3")
+        try:
+            s3_client.head_bucket(Bucket=s3_bucket_name)
+        except Exception:
+            print("S3 Bucket '{}' does not exist".format(s3_bucket_name))
+            sys.exit(1)
+
+        if start_time:
+            start_datetime = parse_date(start_time)
+
+        if end_time:
+            end_datetime = parse_date(end_time)
+
+        # Scan the objects in the bucket
+        s3_object_list = get_objects(s3_client, s3_bucket_name, limit, start_datetime, end_datetime)
+        if limit:
+            s3_object_list = s3_object_list[: min(limit, len(s3_object_list))]
+            print(f"Final: Objects to be scanned: {len(s3_object_list)}")
+        if not report:
+            for key_name in s3_object_list:
+                scan_object(lambda_client, lambda_function_name, s3_bucket_name, key_name)
+        else:
+            export_json(s3_object_list)
 
 
 if __name__ == "__main__":
@@ -129,7 +165,24 @@ if __name__ == "__main__":
     parser.add_argument(
         "--profile", help="AWS profile to use", default="default"
     )
-    parser.add_argument("--limit", type=int, help="The number of records to limit to")
+    parser.add_argument(
+        "--limit", type=int, help="The number of records to limit to"
+    )
+    parser.add_argument(
+        "--start-date", type=int, help="Start time: Epoch timestamp"
+    )
+    parser.add_argument(
+        "--end-date", type=int, help="End time: Epoch timestamp"
+    )
+    parser.add_argument(
+        "--report", action='store_true', help="Just generate a report"
+    )
+    parser.add_argument(
+        "--export-file", type=str, help="Name of the export file to use"
+    )
+
     args = parser.parse_args()
 
-    main(args.lambda_function_name, args.s3_bucket_name, args.profile, args.limit)
+    main(args.lambda_function_name, args.s3_bucket_name, args.profile, 
+        args.limit, start_time=args.start_date, end_time=args.end_date, 
+        report=args.report, export_file=args.export_file)
